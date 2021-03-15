485d1564ef35787802f475026179123b259ecf53
Mersad Shahidi <info@mercads.ir>
2021-03-15 01:31:58 -0700

    New Ping !

--- userge/plugins/tools/ping.py
+++ userge/plugins/tools/ping.py
@@ -31,4 +31,4 @@ async def pingme(message: Message):
         await message.edit('`Pong!`')
         end = datetime.now()
         m_s = (end - start).microseconds / 1000
-        await message.edit(f"**Pong!**\n`{m_s} ms`")
+        await message.edit(f" Ping **Pong!**\n **SELFBOT IS ONLINE** \n >Ping: `{m_s} ms`")
